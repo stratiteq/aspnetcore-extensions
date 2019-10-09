1692c17dd1125486525e642990f9070b6aac73e0
Peter Nylander <coolmalone@users.noreply.github.com>
2019-10-09 12:16:15 +0200

    Added ProblemDetailsWithErrorCode, ProblemDetailsWithErrorCodeCollection and ProblemDetailsWithErrorCodeException (#1)

---
+++ src/Stratiteq.Extensions.AspNetCore/ProblemDetailsExtensions/ProblemDetailsWithErrorCode.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Stratiteq Sweden AB. All rights reserved.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Stratiteq.Extensions.AspNetCore.ProblemDetailsExtensions
+{
+    /// <summary>
+    /// Extends the ProblemDetails class with an error code.
+    /// </summary>
+    public class ProblemDetailsWithErrorCode : ProblemDetails
+    {
+        /// <inheritDoc/>
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProblemDetailsWithErrorCode"/> class.
+        /// </summary>
+        public ProblemDetailsWithErrorCode(string errorCode, string title)
+        {
+            this.ErrorCode = errorCode ?? throw new ArgumentNullException(nameof(errorCode));
+            this.Title = title;
+        }
+
+        /// <summary>
+        /// Gets the unique code for identifying the specific error within this API.
+        /// </summary>
+        public string ErrorCode { get; }
+
+        /// <summary>
+        /// Creates a ProblemDetailsWithErrorCodeException.
+        /// </summary>
+        public ProblemDetailsWithErrorCodeException CreateException() => new ProblemDetailsWithErrorCodeException(this);
+    }
+}
---
+++ src/Stratiteq.Extensions.AspNetCore/ProblemDetailsExtensions/ProblemDetailsWithErrorCodeCollection.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Stratiteq Sweden AB. All rights reserved.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.ObjectModel;
+
+namespace Stratiteq.Extensions.AspNetCore.ProblemDetailsExtensions
+{
+    /// <summary>
+    /// Collection of ProblemDetailsWithErrorCode objects. The error codes are guaranteed to be unique within this collection.
+    /// </summary>
+    public class ProblemDetailsWithErrorCodeCollection : KeyedCollection<string, ProblemDetailsWithErrorCode>
+    {
+        /// <inheritDoc/>
+        protected override string GetKeyForItem(ProblemDetailsWithErrorCode item) => item.ErrorCode;
+    }
+}
